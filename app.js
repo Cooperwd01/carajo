625263a721d18d262f80ba3a17c21a15e68edbee
nhsbsa-Will <william.cooper@nhs.net>
2020-04-21 23:04:39 +0100

    score

--- app.js
+++ app.js
@@ -58,6 +58,186 @@ $(document).ready(function() {
     });
 });
 
+// Player 5
+$(document).ready(function() {
+    $("#a5").keyup(function() {
+        if (this.value === 'lewis' || this.value === 'Lewis' || this.value === 'Eddie Lewis' || this.value === 'Eddie lewis' || this.value === 'eddie Lewis' || this.value === 'eddie lewis' || this.value === 'edward lewis' || this.value === 'Edward Lewis' ) {
+            $("div#play-five").addClass("correct");
+            $("div#play-five").removeClass("incorrect");
+        } else if (this.value === '') {
+            $("div#play-five").removeClass("correct");
+            $("div#play-five").removeClass("incorrect");
+        } else {
+            $("div#play-five").addClass("incorrect");
+        }
+    });
+});
+
+// Player 6
+$(document).ready(function() {
+    $("#a6").keyup(function() {
+        if (this.value === 'gylfi einarsson' || this.value === 'Gylfi einarsson' || this.value === 'Gylfi Einarsson' || this.value === 'gylfi Einarsson' || this.value === 'gylfi' || this.value === 'einarsson' || this.value === 'Gylfi' || this.value === 'Einarsson' || this.value === 'gylfi Einarson' || this.value === 'gilfi' || this.value === 'Einerson' || this.value === 'einerson'  ) {
+            $("div#play-six").addClass("correct");
+            $("div#play-six").removeClass("incorrect");
+        } else if (this.value === '') {
+            $("div#play-six").removeClass("correct");
+            $("div#play-six").removeClass("incorrect");
+        } else {
+            $("div#play-six").addClass("incorrect");
+        }
+    });
+});
+
+// Player 7
+$(document).ready(function() {
+    $("#a7").keyup(function() {
+        if (this.value === 'hayden fox' || this.value === 'hayden foxe' || this.value === 'foxe' || this.value === 'fox' || this.value === 'Fox' || this.value === 'Foxe' || this.value === 'Hayden Fox' || this.value === 'Hayden Foxe' || this.value === 'Hayden fox' ) {
+            $("div#play-seven").addClass("correct");
+            $("div#play-seven").removeClass("incorrect");
+        } else if (this.value === '') {
+            $("div#play-seven").removeClass("correct");
+            $("div#play-seven").removeClass("incorrect");
+        } else {
+            $("div#play-seven").addClass("incorrect");
+        }
+    });
+});
+
+// Player 8
+$(document).ready(function() {
+    $("#a8").keyup(function() {
+        if (this.value === 'Dan Harding' || this.value === 'dan harding' || this.value === 'harding' || this.value === 'Harding' || this.value === 'Daniel Harding' || this.value === 'Dan harding' || this.value === 'daniel harding' ) {
+            $("div#play-eight").addClass("correct");
+            $("div#play-eight").removeClass("incorrect");
+        } else if (this.value === '') {
+            $("div#play-eight").removeClass("correct");
+            $("div#play-eight").removeClass("incorrect");
+        } else {
+            $("div#play-eight").addClass("incorrect");
+        }
+    });
+});
+
+// Player 9
+$(document).ready(function() {
+    $("#a9").keyup(function() {
+        if (this.value === 'teddy lucic' || this.value === 'Teddy lucic' || this.value === 'teddy lucic' || this.value === 'lucic' || this.value === 'Lucic' || this.value === 'Lukic' || this.value === 'teddy lukic' || this.value === 'Teddy lukic' || this.value === 'teddy lukic' || this.value === 'lukic' || this.value === 'Lukic' ) {
+            $("div#play-nine").addClass("correct");
+            $("div#play-nine").removeClass("incorrect");
+        } else if (this.value === '') {
+            $("div#play-nine").removeClass("correct");
+            $("div#play-nine").removeClass("incorrect");
+        } else {
+            $("div#play-nine").addClass("incorrect");
+        }
+    });
+});
+
+// Player 10
+$(document).ready(function() {
+    $("#a10").keyup(function() {
+        if (this.value === 'Paul Okon' || this.value === 'Paul okon' || this.value === 'paul okon' || this.value === 'okon' || this.value === 'Okon' ) {
+            $("div#play-ten").addClass("correct");
+            $("div#play-ten").removeClass("incorrect");
+        } else if (this.value === '') {
+            $("div#play-ten").removeClass("correct");
+            $("div#play-ten").removeClass("incorrect");
+        } else {
+            $("div#play-ten").addClass("incorrect");
+        }
+    });
+});
+
+// Player 11
+$(document).ready(function() {
+    $("#a11").keyup(function() {
+        if (this.value === 'Martin Hiden' || this.value === 'Martin hiden' || this.value === 'martin hiden' || this.value === 'Hiden' || this.value === 'hiden' ) {
+            $("div#play-eleven").addClass("correct");
+            $("div#play-eleven").removeClass("incorrect");
+        } else if (this.value === '') {
+            $("div#play-eleven").removeClass("correct");
+            $("div#play-eleven").removeClass("incorrect");
+        } else {
+            $("div#play-eleven").addClass("incorrect");
+        }
+    });
+});
+
+// Player 12
+$(document).ready(function() {
+    $("#a12").keyup(function() {
+        if (this.value === 'Danny Hay' || this.value === 'Danny hay' || this.value === 'Hay' || this.value === 'hay' || this.value === 'Daniel Hay' || this.value === 'Daniel hay' || this.value === 'daniel hay' ) {
+            $("div#play-twelve").addClass("correct");
+            $("div#play-twelve").removeClass("incorrect");
+        } else if (this.value === '') {
+            $("div#play-twelve").removeClass("correct");
+            $("div#play-twelve").removeClass("incorrect");
+        } else {
+            $("div#play-twelve").addClass("incorrect");
+        }
+    });
+});
+
+// Player 13
+$(document).ready(function() {
+    $("#a13").keyup(function() {
+        if (this.value === 'jacob burns' || this.value === 'Jacob burns' || this.value === 'Jacob Burns' || this.value === 'burns' || this.value === 'Burns' ) {
+            $("div#play-thirteen").addClass("correct");
+            $("div#play-thirteen").removeClass("incorrect");
+        } else if (this.value === '') {
+            $("div#play-thirteen").removeClass("correct");
+            $("div#play-thirteen").removeClass("incorrect");
+        } else {
+            $("div#play-nine").addClass("incorrect");
+        }
+    });
+});
+
+// Player 14
+$(document).ready(function() {
+    $("#a14").keyup(function() {
+        if (this.value === 'Willem Korsten' || this.value === 'Willem korsten' || this.value === 'willem korsten' || this.value === 'korsten' || this.value === 'Korsten' ) {
+            $("div#play-fourteen").addClass("correct");
+            $("div#play-fourteen").removeClass("incorrect");
+        } else if (this.value === '') {
+            $("div#play-fourteen").removeClass("correct");
+            $("div#play-fourteen").removeClass("incorrect");
+        } else {
+            $("div#play-fourteen").addClass("incorrect");
+        }
+    });
+});
+
+// Player 15
+$(document).ready(function() {
+    $("#a15").keyup(function() {
+        if (this.value === 'bruno ribeiro' || this.value === 'Bruno ribeiro' || this.value === 'Bruno Ribeiro' || this.value === 'ribeiro' || this.value === 'Ribeiro' || this.value === 'ribero' || this.value === 'Ribero' ) {
+            $("div#play-fifteen").addClass("correct");
+            $("div#play-fifteen").removeClass("incorrect");
+        } else if (this.value === '') {
+            $("div#play-fifteen").removeClass("correct");
+            $("div#play-fifteen").removeClass("incorrect");
+        } else {
+            $("div#play-fifteen").addClass("incorrect");
+        }
+    });
+});
+
+// Player 16
+$(document).ready(function() {
+    $("#a16").keyup(function() {
+        if (this.value === 'Lloyd Sam' || this.value === 'Lloyd sam' || this.value === 'lloyd sam' || this.value === 'Sam' || this.value === 'sam' || this.value === 'Loyd Sam' || this.value === 'loyd sam' || this.value === 'Loyd sam' ) {
+            $("div#play-sixteen").addClass("correct");
+            $("div#play-sixteen").removeClass("incorrect");
+        } else if (this.value === '') {
+            $("div#play-sixteen").removeClass("correct");
+            $("div#play-sixteen").removeClass("incorrect");
+        } else {
+            $("div#play-sixteen").addClass("incorrect");
+        }
+    });
+});
+
 // update
 $(document).ready(function() {
     $(".lufc-input").keyup(function() {
